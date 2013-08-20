d5eafc9c391aec8f89ecddf3830e101bd10f7764
Caleb Osborne <caleb.t.osborne@gmail.com>
2013-08-20 13:54:39 -0400

    Renamed to Baseline

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Theverb::Application.config.session_store :cookie_store, key: '_theverb_session'
+Baseline::Application.config.session_store :cookie_store, key: '_Baseline_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Theverb::Application.routes.draw do
+Baseline::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   get "/auth/:provider/callback" => "sessions#create"
